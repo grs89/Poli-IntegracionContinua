d573e4f9f5daab72182c0a2903aeb599528a355d
BrahianR7 <80499239+BrahianR7@users.noreply.github.com>
2024-05-26 20:55:46 -0500

    Update database_script.sql
    Se realizan modificaciones a scripts de BD

--- Base de Datos/database_script.sql
+++ Base de Datos/database_script.sql
@@ -1,14 +1,10 @@
 CREATE DATABASE db_consultas;
 
-CREATE TABLE `usuarios` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `nombres` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `apellidos` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `numerodocumento` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `edad` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `genero` int(2) NOT NULL,
-  `correo` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `estado` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1',
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE usuarios (
+  id bigint NOT NULL,
+  nombres varchar(50) NOT NULL,
+  apellidos varchar(50) NOT NULL,
+  numerodocumento varchar(20) NOT NULL,
+  tipo_sangre varchar(3) NOT NULL,
+  es_beneficiario boolean NOT NULL,
+);
